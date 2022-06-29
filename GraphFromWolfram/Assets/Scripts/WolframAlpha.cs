07c924519bb65a425c9e6f56d43334474dfa52b8
DanielLorentz <dlorentz8@gmail.com>
2022-06-29 14:37:10 -0700

    Add methods for tangent line and function from points

--- GraphFromWolfram/Assets/Scripts/WolframAlpha.cs
+++ GraphFromWolfram/Assets/Scripts/WolframAlpha.cs
@@ -11,9 +11,16 @@ using System.Text.RegularExpressions;
 
 public class WolframAlpha : MonoBehaviour
 {
+    //Used in Solve
     public delegate void ObtainPointsEvent(List<float> points);
     public static event ObtainPointsEvent onObtainPoints;
-
+/* //Used in GetTangentFunction
+    public delegate void ObtainTangentEvent(string tangentLine);
+    public static event ObtainTangentEvent onObtainTangent;
+*/ /* //Used in GetFuncitonFromPoints
+    public delegate void ObtainFunctionFromPoints(bool success, string function);
+    public static event ObtainFunctionFromPoints onObtainFunction;
+*/
     WolframAlphaClient client;
     // Start is called before the first frame update
     void Awake()
@@ -54,18 +61,65 @@ public class WolframAlpha : MonoBehaviour
     }
 
     static List<float> cleanResults(string toClean){
-        // string trim = toClean.Replace(" ", "");
-        // char breakOn = ',';
-        // string[] broken = trim.Split(breakOn);
-        // broken[0] = broken[0].Substring(1,broken[0].Length - 1);
-        // broken[broken.Length -1] = broken[broken.Length-1].Substring(0, broken[broken.Length-1].Length -1);
-        // float[] cleaned = new float[broken.Length];
-        // for (int i = 0; i < broken.Length; i++){
-        //     //Debug.Log(str);
-        //     cleaned[i] = Convert.ToSingle(broken[i]);
-        //     Debug.Log(cleaned[i]);
-        // }
+        
         
         return Regex.Replace(toClean, @"{|}|\s", "").Split(',').Select(float.Parse).ToList();;
     }
+/*
+    // GetTangentFunction: Gets the tangent function at x
+    // @param: originalFunction -- string of original function
+    // @param: x                -- x value to get tangent for
+    // @returns: invokes set render function and passes the tangent function to is as a string
+    public async void GetTangentFunction(string originalFunciton, float x){
+        string input = "Tangent line [" + originalFunciton + "] x = " + x;
+        FullResultResponse results = await client.FullResultAsync(input);
+        string resultString = "";
+
+        foreach (Pod pod in results.Pods){
+            if(pod.Title == "Interpolating polynomial"){
+                foreach (SubPod subPod in pod.SubPods){
+                    if(string.IsNullOrEmpty(subPod.Plaintext)){
+                        Debug.Log("<Cannot output in console>");
+                    }else{
+                        resultString = subPod.Plaintext;
+                    }
+                }
+            }
+        }
+        onObtainTangent?.Invoke(resultString);
+
+    }
+*/ /*
+    // GetFunctionFromPoints: Gets a standard y=x function from a series of points
+    // @param: points   -- string of points to derive function for
+    // @returns: success -- invokes pointed c# success function and passes derived function as string
+    // @returns: failure -- invokes pointed c# failure function. equation could not be derived. 
+    public async void GetFunctionFromPoints(string points){
+        string input = "Curve fit {" + points + "}";
+
+        FullResultResponse results = await client.FullResultAsync(input);
+        string resultString = "";
+
+        foreach(Pod pod in results.Pods){
+            if(pod.Title == "Result"){
+                foreach (SubPod subPod in pod.SubPods){
+                    if(string.IsNullOrEmpty(subPod.Plaintext)){
+                        Debug.Log("<Cannot output in console>");
+                    }else{
+                        resultString = subPod.Plaintext;
+                    }
+                }
+            }
+        }
+        if(resultString != ""){
+            onObtainFunction?.Invoke(resultString);
+        }else{
+            notObtainFunction?.Invoke();
+        }
+    }
+*/ /*
+    public async void GetDerivativeFunction(string function){
+        string input = "Derivative [" + function + "]"
+    }
+    */
 }
