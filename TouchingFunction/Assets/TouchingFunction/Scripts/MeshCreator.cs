c73ae6ea1f10b44959abaebd88c4381ac7db1f7b
khushaalkurswani <khushaalkurswani@hotmail.com>
2022-08-03 15:50:28 -0700

    Refactored mesh scaling

--- TouchingFunction/Assets/TouchingFunction/Scripts/MeshCreator.cs
+++ TouchingFunction/Assets/TouchingFunction/Scripts/MeshCreator.cs
@@ -38,23 +38,9 @@ public class MeshCreator : MonoBehaviour
     string cachedFunction;
 
     public void onGraphChanged() {
-        Vector3 meshScale = transform.localScale;
-        Vector3 meshPos = transform.position;
-        
-        double xUnitSpace = graphManScript.GetXUnitSpace();
-        float xScale = (float) xUnitSpace;
-        float xPos = (xScale - 1) * maxVal;
-        Debug.Log("xscale: " + xScale);
-        Debug.Log("xPos: " + xPos);
-
-        double yUnitSpace = graphManScript.GetYUnitSpace();
-        float yScale = (float) yUnitSpace;
-        float yPos = graphAxes.transform.localPosition.y - (yScale / 2);
-        Debug.Log("yscale: " + yScale);
-        Debug.Log("yPos: " + yPos);
-
-        transform.localScale = new Vector3(xScale, yScale, meshScale.z);
-        transform.position = graphAxes.transform.localPosition;
+        createGraphMesh();
+        UpdateMesh();
+        transform.localPosition = graphAxes.transform.localPosition;
     }
 
     public void InitGraphParameters(int min, int max, int width, float increment)
@@ -84,7 +70,7 @@ public class MeshCreator : MonoBehaviour
         createGraphMesh();
         UpdateMesh();
         //GetComponent<PointsCreator>().CreatePoints();
-        onGraphChanged();
+        //onGraphChanged();
 
          //FindObjectOfType<MeshManager>().addMesh(GetComponent<MeshCreator>());
         
@@ -132,11 +118,15 @@ public class MeshCreator : MonoBehaviour
         float xOrigin = 0f;
         float yOrigin = 0f;
         float xVal, yVal;
+
+        float xUnitSpace = (float) graphManScript.GetXUnitSpace();
+        float yUnitSpace = (float) graphManScript.GetYUnitSpace();
+
         
         for (int i = 0; i < MeshPerX * (maxVal - minVal); i++)
         {
-            xVal = xOrigin + minVal + (i * xIncrement);
-            yVal = yOrigin + getY(i);
+            xVal = (xOrigin + minVal + (i * xIncrement)) * xUnitSpace;
+            yVal = (yOrigin + getY(i)) * yUnitSpace;
             for (int j = 0; j < 2; j++)
             {
                 vertices[2 * i + j] = new Vector3(xVal, yVal, zVals[j]);
@@ -155,6 +145,7 @@ public class MeshCreator : MonoBehaviour
              triangles[6 * i + 5] = 2 * (i + 1);
             
         }
+        transform.localPosition = graphAxes.transform.localPosition;
     }
 
     public bool isEmpty()
